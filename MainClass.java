dd6fea9b5bf2d02cb24ba4e67f42bf3324f2bb40
batool20 <batoolmohammed705@yahoo.com>
2023-12-03 19:42:07 +0200

    Adding feature: selection Sort method

--- MainClass.java
+++ MainClass.java
@@ -8,5 +8,22 @@ public class MainClass {
         MergeSort3way Merge = new MergeSort3way();
 >>>>>>> 59550ccb75373caab21cb36e687e3a811d59166c
     }
-    
+     private static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
