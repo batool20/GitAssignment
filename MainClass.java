167752638cd2435bd49688707bcbfa3dc2e33d14
batool20 <batoolmohammed705@yahoo.com>
2023-12-03 19:27:46 +0200

    Adding feature: Insertion Sort method

--- MainClass.java
+++ MainClass.java
@@ -1,7 +1,26 @@
-
+import java.util.Arrays;
 public class MainClass {
     public static void main(String[] args) {
         MergeSort3way Merge = new MergeSort3way();
+        int[] numbers = {4, 2, 7, 1, 9, 5, 3, 8, 6};
+
+        System.out.println("Array before sorting: " + Arrays.toString(numbers));  
+        insertionSort(numbers);
+        System.out.println("Array after sorting: " + Arrays.toString(numbers));
+    }
+    private static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
     
 }
